3e63aa726619b2342fb3bec6ea6a2b11141e7fa5
Dabble <ddabble@users.noreply.github.com>
2018-10-04 12:01:51 +0200

    Whitespace cleanup

--- project3/components_3rd-party/todoapp-reactnative/TodoModel.js
+++ project3/components_3rd-party/todoapp-reactnative/TodoModel.js
@@ -1,9 +1,9 @@
 class TodoModel {
-  constructor(title, completed) {
-    this.title = title;
-    this.completed = completed || false;
-    this.createdAt = new Date();
-  }
+    constructor(title, completed) {
+        this.title = title;
+        this.completed = completed || false;
+        this.createdAt = new Date();
+    }
 }
 
 module.exports = TodoModel;
