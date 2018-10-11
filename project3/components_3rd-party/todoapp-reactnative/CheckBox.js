3201b57b0a1751355e260c2579d04e950825ea19
Dabble <ddabble@users.noreply.github.com>
2018-10-11 11:10:40 +0200

    Fixed checkboxes not properly updating

--- project3/components_3rd-party/todoapp-reactnative/CheckBox.js
+++ project3/components_3rd-party/todoapp-reactnative/CheckBox.js
@@ -4,18 +4,15 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 class CheckBox extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            data: this.props.data
-        };
     }
 
     render() {
-        let iconName = this.state.data.completed ? 'check-box' : 'check-box-outline-blank';
+        let iconName = this.props.data.completed ? 'check-box' : 'check-box-outline-blank';
         let color = this.props.color || '#000';
 
         return (
             <Icon.Button
-                data={this.state.data}
+                data={this.props.data}
                 name={iconName}
                 backgroundColor='rgba(0,0,0,0)'
                 color={color}
