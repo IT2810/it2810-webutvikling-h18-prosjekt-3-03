10e7eee02ec561048cd90fdb24f58d9f8bd2c147
Katrinhh <katrine.hveding@hotmail.com>
2018-10-17 08:47:53 +0200

    Made a list of activities you can filter by date in the calendar. #1 #19

---
+++ project3/components/Activity.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+export default class Activity extends React.Component {
+
+    /*
+    Needs:
+    -dateString
+    -description
+    -functionality for editing the activity
+    
+
+    */
+    constructor() {
+        super();
+        this.state = {
+            date: '',
+            description: ''
+        }
+    }
+
+    render() {
+        return (null);
+    }
+
+}
--- project3/components/CalendarComponent.js
+++ project3/components/CalendarComponent.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Calendar, CalendarList } from 'react-native-calendars';
-
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { CalendarList } from 'react-native-calendars';
+import { asyncStorage } from 'react-native';
+import { List, ListItem, FlatList } from 'react-native-elements';
+ 
 export default class CalendarComponent extends React.Component {
+
+    /*
+    Needs:
+    -currently selected date (selected)
+    -list with activities connected to each date
+        {date: (description1, description2)}
+    -asyncStorage for stored activities
+    -textinput to add activity
+
+    function: 
+    -when onDayPress() is triggered for a date it should trigger another function
+    -this function should go through activityList and display all activities that match datestring 
+    */
+
+    list = [
+        {
+         date: '2018-10-09',
+         text: 'An activity'},
+        {
+         date: '2018-10-16',
+         text: 'A second activity'}];
 
     constructor(props) {
         super(props);
         this.state = {
-            selected: '2018-10-09'
+            selected: '2018-10-09',
+            markedDates: [
+                {'': 'An activity'}
+            ]
         };
     }
+
+
+    getActivities() {
+        // iterates over list and shows items for selected date
+    }
+
+    
 
     render () {
         return (
@@ -24,10 +57,24 @@ export default class CalendarComponent extends React.Component {
                         [this.state.selected]: {selected: true, marked: true, selectedColor: 'green'},
                       }}
                 />
+              
                 <View style={styles.activity}>
                     <Text>Activity</Text>
                     <Text>{this.state.selected}</Text>
+                    <List>
+                        {
+                            this.list.filter((item) => (item.date == this.state.selected)).map((item) => (
+                                <ListItem
+                                key={item.text}
+                                title={item.text}
+                                // hides right arrow, can use for edit?
+                                hideChevron={true} 
+                                />
+                            ))
+                        }
+                    </List>
                 </View>
+
             </View>
         );
        
