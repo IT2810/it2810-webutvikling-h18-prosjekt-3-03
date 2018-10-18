c9aee0301903fd46fc445f9bd00dcaa764c8da82
Dabble <ddabble@users.noreply.github.com>
2018-10-18 17:42:20 +0200

    Added 2nd ListViewItem test

--- project3/components_3rd-party/todoapp-reactnative/__tests__/ListViewItem-test.js
+++ project3/components_3rd-party/todoapp-reactnative/__tests__/ListViewItem-test.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import {View} from 'react-native';
 import renderer from 'react-test-renderer';
 import ListViewItem from '../ListViewItem';
 import TodoModel from '../TodoModel';
 
 it('renders correctly', () => {
     const tree = renderer
-        .create(<ListViewItem data={new TodoModel("Test title", false, new Date(0))}
-                              dataIndex={0}
-        />)
+        .create(<View>
+            <ListViewItem data={new TodoModel("Test title", false, new Date(0))}
+                          dataIndex={0}/>
+            <ListViewItem data={new TodoModel("Test title 2", true, new Date(0))}
+                          dataIndex={0}/>
+        </View>)
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
